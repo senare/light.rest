358a3241cc2d0552aba00f781da5902367d58040
senare <senare@gmail.com>
2015-11-06 23:54:10 +0100

    Update

--- src/main/java/light/rest/LightService.java
+++ src/main/java/light/rest/LightService.java
@@ -1,19 +1,73 @@
 package light.rest;
 
-import static spark.Spark.*;
+import static spark.Spark.get;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class LightService {
+
+    private static final ScheduledExecutorService service = Executors.newScheduledThreadPool(1);
+
     public static void main(String[] args) {
-        get("/on/:target", (req, res) -> {
-            return "On " + req.params(":target") + " => " + executeCommand("/usr/bin/tdtool -n 2") + "";
+
+        service.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                // TODO CHECK BAMBOO
+            }
+        }, 1, 1, TimeUnit.MINUTES);
+
+        get("/green", (req, res) -> {
+            greenOn();
+            redOff();
+            return " => green";
         });
 
-        get("/off/:target", (req, res) -> {
-            return "Off " + req.params(":target") + " => " + executeCommand("/usr/bin/tdtool -f 2") + "";
+        get("/red", (req, res) -> {
+            redOn();
+            greenOff();
+            return " => red";
         });
+    }
+
+    private static void greenOff() {
+        service.schedule(new Runnable() {
+            @Override
+            public void run() {
+                executeCommand("/usr/bin/tdtool -f 3");
+            }
+        }, 1, TimeUnit.SECONDS);
+    }
+
+    private static void redOn() {
+        service.schedule(new Runnable() {
+            @Override
+            public void run() {
+                executeCommand("/usr/bin/tdtool -n 2");
+            }
+        }, 1, TimeUnit.SECONDS);
+    }
+
+    private static void redOff() {
+        service.schedule(new Runnable() {
+            @Override
+            public void run() {
+                executeCommand("/usr/bin/tdtool -f 2");
+            }
+        }, 1, TimeUnit.SECONDS);
+    }
+
+    private static void greenOn() {
+        service.schedule(new Runnable() {
+            @Override
+            public void run() {
+                executeCommand("/usr/bin/tdtool -n 3");
+            }
+        }, 1, TimeUnit.SECONDS);
     }
 
     private static String executeCommand(String command) {
